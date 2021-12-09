a51fc372a36b44f693ee92d06f1637725df907c1
jangsoo2 <92726957+jangsoo2@users.noreply.github.com>
2021-12-09 14:21:22 +0900

    Delete lib.js

--- lib.js
+++
@@ -1,3 +0,0 @@
-function load(url){window.open(url,"mywin","left=1000,top=1000, width=1000, height=1000");}
-function over(obj){obj.src="교육자료전/homepage.png";}
-function out(obj){obj.src="교육자료전/타이틀_자료.png";}
